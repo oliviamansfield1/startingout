b07be74a91f02dbbe5e335943715c0f30e6f6b72
Fergus Barratt <fergus.barratt00@gmail.com>
2020-01-22 17:41:09 +0000

    this is a commit message

--- familylist.js
+++ familylist.js
@@ -1,4 +1,4 @@
-var Family = ["Dad", "Mum", "Louis", "Babies", "Fergus", "Misty"]
+var Family = ["Dad", "Mum", "Louis", "Babies", "Fergus", "Misty", "Zac"]
 for (i = 3; i<=5 ; i=i+1) {
   console.log(Family[i])
 }
